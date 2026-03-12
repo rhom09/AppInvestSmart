c500d26749643fd604d5031a06298e004088346b
Rhom09 <rhom0909@gmail.com>
2026-03-11 21:57:58 -0300

    fix: use default import for YahooFinance class

--- backend/src/services/yahoo.service.ts
+++ backend/src/services/yahoo.service.ts
@@ -1,4 +1,4 @@
-import { YahooFinance } from 'yahoo-finance2'
+import YahooFinance from 'yahoo-finance2'
 const yahooFinance = new YahooFinance()
 
 const toYahoo = (ticker: string) => `${ticker}.SA`
